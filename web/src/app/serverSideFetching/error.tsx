e675b7028c040f545b65ce27246f17298f0a40ac
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2023-09-28 14:27:39 -0300

    feat: next built-in error boundary

--- web/src/app/serverSideFetching/error.tsx
+++ web/src/app/serverSideFetching/error.tsx
@@ -1,16 +1,23 @@
 'use client';
 
+import { useRouter } from "next/navigation";
+
 export default function Error({
   error, reset
 }:{
   error: Error & { digest?: string }
   reset: () => void
 }) {
+  const router = useRouter();
+  const redirectToMainMenu = () => router.push('/')
+
 
   return (
     <div>
       <p>something went wrong :( sorry ;-;</p>
       <p>{error.message}</p>
+      <button>try again</button>
+      <button>go back to main menu</button>
     </div>
   )
 }
