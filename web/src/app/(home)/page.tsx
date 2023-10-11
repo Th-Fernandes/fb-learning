6da85a085b11d09f732e4949b179ed8abce8f727
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2023-10-11 17:48:25 -0300

    feat: using root route page with parentesis approach

--- web/src/app/(home)/page.tsx
+++ web/src/app/(home)/page.tsx
@@ -39,7 +39,6 @@ export default function Home() {
                 width={64}
                 height={64}
               />
-
               <div>
               {loggedInUser && loggedInUser.displayName}
             </div>
