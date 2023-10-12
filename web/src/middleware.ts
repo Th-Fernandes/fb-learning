315b7185423aa2c3eebf797cae06b81ee7f0f6a2
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2023-10-12 12:13:47 -0300

    feat: unauthorize route handlers access on client side

---
+++ web/src/middleware.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(req: NextRequest) {
+  const { pathname } = req.nextUrl;
+  
+  const isRequestMadeByClient = !req.headers.get("referer")?.includes('http://localhost:3000')
+
+  if (pathname.startsWith(`/api/`) && isRequestMadeByClient) 
+    return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
+
+  return NextResponse.next()
+
+}
+
+export const config = {
+  matcher: ['/((?!_next|fonts|examples|svg|[\\w-]+\\.\\w+).*)'],
+}
